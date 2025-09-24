579626129830f42199888522514935445b9be431
Gustavo barcaro <gubarcaro@gmail.com>
2025-09-23 23:19:38 -0300

    make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -9,12 +9,38 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
+  try {
+    const res = await client.query(queryObject);
+    return res;
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.end();
+  }
+}
 
-  const res = await client.query(queryObject);
-  await client.end();
-  return res;
+async function getVersion() {
+  const response = await query("SHOW server_version;");
+  return response.rows[0].server_version;
+}
+
+async function getMaxConnections() {
+  const response = await query("SHOW max_connections;");
+  return Number(response.rows[0].max_connections);
+}
+
+async function getCurrentConnections() {
+  const databaseName = process.env.POSTGRES_DB;
+  const response = await query({
+    text: `SELECT COUNT(*)::int AS current_connections FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  return response.rows[0].current_connections;
 }
 
 export default {
   query,
+  getVersion,
+  getMaxConnections,
+  getCurrentConnections,
 };
