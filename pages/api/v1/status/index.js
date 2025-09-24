0ee4be5555d66d4ee6d6cabf3cf42c011b5c8ab2
Gustavo barcaro <gubarcaro@gmail.com>
2025-09-23 23:19:56 -0300

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,16 @@
-import database from "infra/database.js"
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;")
-  console.log(result.rows)
+  const updatedAt = new Date().toISOString();
   response.status(200).json({
-    chave: "valor",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: await database.getVersion(),
+        max_connections: await database.getMaxConnections(),
+        current_connections: await database.getCurrentConnections(),
+      },
+    },
   });
 }
 
